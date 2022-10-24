fd9cfb87ee0e11902bf9d7a28e0dcc786002ba5c
PC <kim000183@naver.com>
2022-10-24 21:10:40 +0900

    puppeteer 데이터 크롤링

--- index.js
+++ index.js
@@ -1,16 +1,28 @@
 const puppeteer = require('puppeteer');
-
+const cheerio = require('cheerio');
 (async() => {
   const browser = await puppeteer.launch({
     headless: false
   });
 
   const page = await browser.newPage();
-
   await page.setViewport({
     width: 1920,
-    height: 768
+    height: 1000
   });
   
   await page.goto('https://ownerclan.com/V2/product/specialProducts.php?no=top100');
+  const content = await page.content();
+  const $ = cheerio.load(content);
+  const lists = $("#productList-content > li");
+
+  lists.each((id, list) => {
+    const name = $(list).find("div.list_st2 > p.new_title02 > a").text();
+    const selfcode = $(list).find("div.list_st2 > p.pro_code").text();
+    const price = $(list).find("div.list_st2 > p.price2").text();
+    console.log({
+      id, name, selfcode, price
+    });
+  });
+  browser.close();
 })();
