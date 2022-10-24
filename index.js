15fea9933cc03d713f98d3276c83ecb340088cb1
PC <kim000183@naver.com>
2022-10-24 20:39:35 +0900

    주석제거

--- index.js
+++ index.js
@@ -1,16 +1,12 @@
-// puppeteer을 가져온다.
 const puppeteer = require('puppeteer');
 
 (async() => {
-  // 브라우저를 실행한다.
-  // 옵션으로 headless모드를 끌 수 있다.
   const browser = await puppeteer.launch({
     headless: false
   });
 
-  // 새로운 페이지를 연다.
   const page = await browser.newPage();
-  // 페이지의 크기를 설정한다.
+
   await page.setViewport({
     width: 1920,
     height: 768
