676c86af67a54ccc14b23d6869e8022e41d01199
PC <kim000183@naver.com>
2022-10-31 20:51:37 +0900

    id 수정

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
-(async() => {
+(async () => {
   const browser = await puppeteer.launch({
     headless: false
   });
@@ -10,16 +10,17 @@ const cheerio = require('cheerio');
     width: 1920,
     height: 1000
   });
-  
+
   await page.goto('https://ownerclan.com/V2/product/specialProducts.php?no=top100');
   const content = await page.content();
   const $ = cheerio.load(content);
   const lists = $("#productList-content > li");
 
-  lists.each((id, list) => {
+  lists.each((count, list) => {
     const name = $(list).find("div.list_st2 > p.new_title02 > a").text();
     const selfcode = $(list).find("div.list_st2 > p.pro_code").text();
     const price = $(list).find("div.list_st2 > p.price2").text();
+    const id = count + 1
     console.log({
       id, name, selfcode, price
     });
