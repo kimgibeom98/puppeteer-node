d15bb8d036c9bd92a108cc1b7a82558c2e2aaf43
PC <kim000183@naver.com>
2022-10-31 21:15:16 +0900

    크롤링한 데이터로 JSON파일 만들기 진행중

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
 const puppeteer = require('puppeteer');
 const cheerio = require('cheerio');
+const fs = require('fs');
+let data = [];
 (async () => {
   const browser = await puppeteer.launch({
     headless: false
@@ -17,13 +19,13 @@ const cheerio = require('cheerio');
   const lists = $("#productList-content > li");
 
   lists.each((count, list) => {
+    const id = count + 1;
     const name = $(list).find("div.list_st2 > p.new_title02 > a").text();
     const selfcode = $(list).find("div.list_st2 > p.pro_code").text();
     const price = $(list).find("div.list_st2 > p.price2").text();
-    const id = count + 1;
-    console.log({
-      id, name, selfcode, price
-    });
+    data.push({id, name, selfcode, price});
   });
   browser.close();
+  const dataJSON = JSON.stringify(data);
+  console.log(dataJSON);
 })();
