a36e0baf43bb0f3a1990132a8a0e778b312dc376
PC <kim000183@naver.com>
2022-11-03 23:26:51 +0900

    master - main 변경

--- index.js
+++ index.js
@@ -1,4 +1,5 @@
 const puppeteer = require('puppeteer');
+const cheerio = require('cheerio');
 const fs = require('fs');
 const crawlingData = [];
 (async () => {
@@ -13,21 +14,20 @@ const crawlingData = [];
   });
 
   await page.goto('https://ownerclan.com/V2/product/specialProducts.php?no=top100');
+  const content = await page.content();
+  const $ = cheerio.load(content);
+  const lists = $("#productList-content > li");
 
-  const ehList = await page.$$("#productList-content > li");
-
-  for (const eh of ehList) {
-    const id = await eh.$eval('div.list_st2 > div.new_img > span.img > div > img', (el) => {
-      const srcCut = el.src.split('/');
-      return Number(srcCut[8].substring(11, srcCut[8].length - 4));
-    });
-    const name = await eh.$eval('div.list_st2 > p.new_title02 > a', (el) => el.innerText);
-    const selfcode = await eh.$eval('div.list_st2 > p.pro_code', (el) => el.innerText);
-    const price = await eh.$eval('div.list_st2 > p.price2', (el) => el.innerText);
-
+  lists.each((count, list) => {
+    const targetSrc = $(list).find('div.list_st2 > div.new_img > span.img > div > img').attr('src');
+    const srcCut = targetSrc.split('/');
+    const id = Number(srcCut[6].substring(11, srcCut[6].length - 4));
+    const name = $(list).find("div.list_st2 > p.new_title02 > a").text();
+    const selfcode = $(list).find("div.list_st2 > p.pro_code").text();
+    const price = $(list).find("div.list_st2 > p.price2").text();
     crawlingData.push({ id, name, selfcode, price });
-  }
+  });
+  browser.close();
   const dataJSON = JSON.stringify(crawlingData, null, 2);
   fs.writeFileSync('crawling-json.json', dataJSON);
-  browser.close();
 })();
